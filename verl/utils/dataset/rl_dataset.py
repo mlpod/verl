5204741b2b6230258388d95338a89dc72355d458
Wayne <58224952+mlpod@users.noreply.github.com>
2025-06-09 12:54:11 +0800

    Update rl_dataset.py

--- verl/utils/dataset/rl_dataset.py
+++ verl/utils/dataset/rl_dataset.py
@@ -126,10 +126,14 @@ class RLHFDataset(Dataset):
             self.data_files[i] = copy_to_local(src=parquet_file, cache_dir=self.cache_dir, use_shm=self.use_shm)
 
     def _read_files_and_tokenize(self):
+        def process_example(example):
+            example["extra_info"]["question"] = example["prompt"][-1]['content']
+            return example
         dataframes = []
         for parquet_file in self.data_files:
             # read parquet files and cache
             dataframe = datasets.load_dataset("parquet", data_files=parquet_file)["train"]
+            dataframe = dataframe.map(process_example)
             dataframes.append(dataframe)
         self.dataframe: datasets.Dataset = datasets.concatenate_datasets(dataframes)
 
